f9eb8d1e8d5bfb0272570b5811f486822c69dbdb
Oswald Toma <oswald.toma@gmail.com>
2020-06-22 21:38:15 +0200

    initial commit

---
+++ Forgets/Appointment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forgets
+{
+    class Appointment : IScheduleRecord
+    {
+        public DateTime getEndTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime getStartTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Forgets/IScheduleRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forgets
+{
+    interface IScheduleRecord
+    {
+        string getName();
+        string getLocation();
+        DateTime getStartTime();
+        DateTime getEndTime();
+    }
+}
